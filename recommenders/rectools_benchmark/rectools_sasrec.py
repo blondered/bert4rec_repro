3b2238204aaeb28314db3527bf32f2dbbcd1e702
Daria Tikhonovich <daria.m.tikhonovich@gmail.com>
2025-01-14 16:53:10 +0300

    trainer

--- recommenders/rectools_benchmark/rectools_sasrec.py
+++ recommenders/rectools_benchmark/rectools_sasrec.py
@@ -1,6 +1,16 @@
 from rectools.models.sasrec import SASRecModel
 from .rectools_model import RectoolsRecommender
+from pytorch_lightning import Trainer
 
 class RectoolsSASRec(RectoolsRecommender):
     def _init_model(self):
-        self.model = SASRecModel(epochs=1, verbose=1, deterministic=True)
+        trainer = Trainer(
+            max_epochs=1,
+            min_epochs=1,
+            deterministic=True,
+            enable_progress_bar=True,
+            enable_model_summary=True,
+            logger=True,
+            accelerator="cpu"  # TODO: change
+        )
+        self.model = SASRecModel(epochs=1, verbose=1, deterministic=True, trainer=trainer)
