2d622b7e2ca085fa87c81499f3331911daed7bfd
Daria Tikhonovich <dmtikhono1@mts.ru>
2025-01-20 18:15:40 +0300

    dataset

--- recommenders/rectools/transformers.py
+++ recommenders/rectools/transformers.py
@@ -1,5 +1,6 @@
 from rectools.models.sasrec import SASRecModel, TransformerModelBase
 from rectools.models.bert4rec import BERT4RecModel
+from rectools.dataset import Dataset
 from .rectools_model import RectoolsRecommender
 from pytorch_lightning import Trainer
 import typing as tp
