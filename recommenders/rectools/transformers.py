3fced9f9d9f24880ef6caaa5b816791eeaa1bafb
blondered <nykenott@gmail.com>
2025-02-03 18:00:15 +0300

    returned old val code

--- recommenders/rectools/transformers.py
+++ recommenders/rectools/transformers.py
@@ -127,25 +127,25 @@ def get_trainer(epochs, callbacks, min_epochs: tp.Optional[int]=None):
 class RectoolsSASRecValidated(RectoolsTransformer):
     def _init_model(self, model_config: tp.Optional[ModelConfig], epochs:int = 1):
         
-        # def get_trainer_sasrec():
-        #     last_epoch_ckpt = ModelCheckpoint(filename="sasrec_last_epoch_{epoch}")
-        #     least_val_loss_ckpt = ModelCheckpoint(
-        #         monitor=SASRecModel.val_loss_name,
-        #         mode="min",
-        #         filename="sasrec_best_val_loss_{epoch}-{val_loss:.2f}",
-        #     )
-        #     early_stopping_val_loss = EarlyStopping(
-        #         monitor=SASRecModel.val_loss_name,
-        #         mode="min",
-        #         patience=20,
-        #     )
-        #     callbacks = [last_epoch_ckpt, least_val_loss_ckpt, early_stopping_val_loss]
-        #     return get_trainer(epochs, callbacks, min_epochs=1)
+        def get_trainer_sasrec():
+            last_epoch_ckpt = ModelCheckpoint(filename="sasrec_last_epoch_{epoch}")
+            least_val_loss_ckpt = ModelCheckpoint(
+                monitor=SASRecModel.val_loss_name,
+                mode="min",
+                filename="sasrec_best_val_loss_{epoch}-{val_loss:.2f}",
+            )
+            early_stopping_val_loss = EarlyStopping(
+                monitor=SASRecModel.val_loss_name,
+                mode="min",
+                patience=20,
+            )
+            callbacks = [last_epoch_ckpt, least_val_loss_ckpt, early_stopping_val_loss]
+            return get_trainer(epochs, callbacks, min_epochs=1)
         
         def get_val_mask_func(interactions: pd.DataFrame):
             return leave_one_out_mask_for_users(interactions, val_users = self.val_users)
-        def get_trainer_sasrec():
-            return self.get_trainer_with_val_loss_ckpt("sasrec", epochs)
+        # def get_trainer_sasrec():
+        #     return self.get_trainer_with_val_loss_ckpt("sasrec", epochs)
         
         self.model = SASRecModel(epochs=epochs, verbose=1, deterministic=True, get_trainer_func=get_trainer_sasrec, get_val_mask_func=get_val_mask_func, **SASREC_DEFAULT_PARAMS)
     
