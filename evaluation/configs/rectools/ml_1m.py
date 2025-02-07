7623ce2dc204b1d8e61d66efe937e79313827f9a
Daria Tikhonovich <dmtikhono1@mts.ru>
2025-02-07 14:31:00 +0300

    fixed configs

--- evaluation/configs/rectools/ml_1m.py
+++ evaluation/configs/rectools/ml_1m.py
@@ -1,9 +1,9 @@
 from aprec.evaluation.split_actions import LeaveOneOut
-
 from aprec.recommenders.rectools.transformers import RectoolsBERT4RecRecallValidated, RectoolsSASRecRecallValidated
 from aprec.evaluation.metrics.mrr import MRR
 from aprec.evaluation.metrics.ndcg import NDCG
 from aprec.evaluation.metrics.recall import Recall
+from aprec.recommenders.bert4recrepro.b4vae_bert4rec import B4rVaeBert4Rec
 from aprec.recommenders.filter_seen_recommender import FilterSeenRecommender
 from aprec.evaluation.samplers.pop_sampler import PopTargetItemsSampler
 
@@ -31,7 +31,14 @@ def bert4rec_recall_val_rt():
 def sasrec_recall_val_rt():
     return RectoolsSASRecRecallValidated(filter_seen=FILTER_SEEN, random_state=RANDOM_STATE, epochs=EPOCHS)
 
+def b4rvae_bert4rec(epochs=EPOCHS):
+    model = B4rVaeBert4Rec(epochs=epochs)
+    if FILTER_SEEN:
+        return FilterSeenRecommender(model)
+    return model
+
 RECOMMENDERS = {
     "sasrec_recall_val_rt": sasrec_recall_val_rt,
     "bert4rec_recall_val_rt": bert4rec_recall_val_rt,
+    "b4vae_bert4rec": b4rvae_bert4rec,
 }
